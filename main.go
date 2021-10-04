6f43f375e52aaeb1df36cdf47b86ceeb014ad07d
shatheesh17 <36799558+shatheesh17@users.noreply.github.com>
2021-10-04 19:45:36 +0530

    new card function

--- main.go
+++ main.go
@@ -1,16 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
+	cards := newCard()
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-}
-
-func newCard() string {
-	return "Five of Diamonds"
+	cards.print()
 }
