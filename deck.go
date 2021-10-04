a648b23ff73837e8c8a0fc13f7e7d8d6ec6bf502
shatheesh17 <36799558+shatheesh17@users.noreply.github.com>
2021-10-04 19:17:21 +0530

    added type deck of string slice

---
+++ deck.go
@@ -0,0 +1,6 @@
+package main
+
+// Create a new type called 'deck'
+// which is a slice of strings
+
+type deck []string
--- main.go
+++ main.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	cards := deck{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
